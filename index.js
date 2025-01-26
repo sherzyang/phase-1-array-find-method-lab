6c0f0c324244f20664aac7d00f3114140c2247fb
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-25 21:45:38 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record){
+    function dolphin (record){return record.result === "W"};
+    let obj = record.find(dolphin);
+    if (obj === undefined) {
+        return undefined;
+    }
+    else return obj.year;
+
+}   
+
+
